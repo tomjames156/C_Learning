c63e4e9fc0bdfff469c6e7e4c5732a7385203799
tomisin <tomjames156@gmail.com>
2023-11-07 21:19:44 +0100

    scratched the surface about functions

--- strings.c
+++ strings.c
@@ -1,12 +1,17 @@
 #include <stdio.h>
 
+void greet(){
+    printf("Hey! How are you doing?");
+}
+
 int main(){
     char introduction[] = "I am Daenerys Stormborn, Breaker of Chains, Mother of Dragons e.t.c";
     char fullName[] = {'D', 'a', 'n', 'y', ' ', 'T', 'a', 'r', 'g', 's', '\0'};
 
 
     printf("%s", introduction);
-    printf(" or %s for short", fullName);
+    printf(" or %s for short\n", fullName);
+    greet();
     printf("\n");
     return 0;
 }
