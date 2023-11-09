fb4994d7f4fddd81667535508b08bf761aef5e14
tomisin <tomjames156@gmail.com>
2023-11-09 22:45:12 +0100

    added a program that calculates factorial using loops

---
+++ factorial.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int factorial(int num){
+    int i;
+    int answer = 1;
+    for(i = 1; i <= num; i++){
+        answer *= i;
+    }
+
+    return answer;
+}
+
+int main(){
+    int num;
+    printf("Find the factorial of any number\nEnter a number below\n=> ");
+    scanf("%d", &num);
+
+    printf("\n%d! = %d\n", num, factorial(num));
+    return 0;
+}
