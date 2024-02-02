b32c4d4453d0aab0b69b2ca533f3a8a5117b9d8c
tomisin <tomjames156@gmail.com>
2024-02-02 10:36:49 +0100

    learnt string basics

--- strings.c
+++ strings.c
@@ -1,9 +1,33 @@
 #include <stdio.h>
 
-int main(){
-    char name[20];
-    printf("Enter your name");
-    scanf("%s", &name);
-    printf("Your name is %s", name);
-    return 0;
-}
+// String Introduction
+
+// int main(){
+//   char my_name[4] = "Tomi"; // this actually takes 6 spaces, 5 for the characters and 1 for the null terminator
+//   char hobby[] = "Programming";
+//   char user[8];
+//   int userAge;
+
+//   char my_chars[4] = {'T', 'o', 'm', 'i'}; // an array of individual chars is not a string so the assignment operator works 
+
+//   // my_chars[2] = 'n';
+
+//   printf("My name is %s and I love %s\n", my_name, hobby);
+//   printf("My name contains 1 %c\n", my_chars[2]);
+
+//   printf("Hey there!, What's your name?\n=> ");
+//   scanf("%s", user);
+//   printf("\nHi there %s. How is your day going?\n", user);
+
+//   printf("How old are you %s\n=> ", user);
+//   scanf("%i", &userAge);
+
+//   if(userAge >= 18){
+//     printf("\nWow, you're an adult. That's hard work");
+//   }else{
+//     printf("\nWow, you're still a child. Enjoy your childhood");
+//   }
+  
+
+//   return 0;
+// }
