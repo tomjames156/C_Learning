15067863baae0a8dbbf6f4475c6a798ff8a90802
tomisin <tomjames156@gmail.com>
2024-02-03 09:36:10 +0100

    revised on strings

--- strings.c
+++ strings.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 
 // String Introduction
 
@@ -31,3 +32,105 @@
 
 //   return 0;
 // }
+
+// gets(), fgets() and puts()
+
+// int main(){
+//   char sibling[7];
+//   printf("What is your brother's name? : ");
+//   // gets(sibling);
+//   fgets(sibling, 7, stdin);
+//   printf("%s is such a cool name", sibling);
+
+//   return 0;
+// }
+
+// String passed to a function
+// void welcomeUser(char fname[], char lname[]);
+
+// void welcomeUser(char fname[], char lname[]){
+//   if(strlen(fname) == 7 && fname[0] == 'T' && fname[6] == 'n'){
+//     printf("Welcome back to the lab Dr %s Akinwande", fname);
+//   }else{
+//     printf("Access Denied");
+//   }
+// }
+
+// int main(){
+//   char user[10];
+//   char userLname[10];
+//   printf("Who are you please?\n=> ");
+//   scanf("%s", user);
+//   scanf("%s", userLname);
+//   printf("%zu", strlen(user)); // checks string length
+//   welcomeUser(user, userLname);
+
+//   return 0;
+// }
+
+
+// int main(){
+//   char intro[100] = "My name is Dr Akinwande Tomisin. ", more[50] = "I like innovation";
+//   char myIntro[100];
+//   char userIntro[70];
+//   char moreOfUser[50];
+//   strcat(intro, more);
+
+//   strcpy(myIntro, intro);
+
+//   printf("%s and I'm an investor\n", myIntro);
+
+//   puts(intro);
+//   // puts(more);
+
+//   printf("What about you? Who are you and what do you like? : ");
+//   fgets(userIntro, 70, stdin);
+//   fgets(moreOfUser, 50, stdin);
+
+//   strcat(userIntro, moreOfUser);
+//   puts(userIntro);
+//   puts(moreOfUser);
+
+//   return 0;
+// }
+
+// void confirmPassword(char pass[]);
+
+// void confirmPassword(char pass[]){
+//   char myPass[15] = "password123";
+  
+//   if((strcmp(pass, myPass)) == 0){
+//     printf("ACCESS GRANTED.\nWelcome back Dr. Akinwande");
+//   }else{
+//     printf("ACCESS DENIED. You have 2 more tries");
+//   }
+// }
+
+// int main(){
+//   char pass[15];
+//   printf("Enter your password : ");
+//   scanf("%s", pass);
+//   confirmPassword(pass);
+
+//   return 0;
+// }
+
+int main(){
+  char user[4] = "Tomi";
+  char reqUser[5];
+
+  printf("Please confirm your name to access my app\n=> ");
+  fgets(reqUser, 5, stdin);
+  // scanf("%s", reqUser);
+
+  int result = strcmp(user, reqUser);
+  printf("Result: %i", result);
+
+  if(result == 0){
+    printf("\nWelcome back Dr Tomisin");
+  }else{
+    printf("\nNEW USER!!!\nWelcome to EdPower %s", reqUser);
+  }
+
+  return 0;
+}
