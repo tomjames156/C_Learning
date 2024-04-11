2b36d918609244d84439fabba4e4667a72ac00ce
tomisin <tomjames156@gmail.com>
2024-04-11 16:39:29 +0100

    added names program

---
+++ CS50/names.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+
+int len(char arr[]);
+
+int main(){
+    char names[5][10] = {"Chad", "Brad", "Jerry", "Brad", "Dekan"};
+
+    for(int i = 0; i < 5; i++){
+        if(strcmp(names[i], "Jerry") == 0){
+            printf("Found");
+            return 0;
+        }else{
+            printf("%d\n", strcmp(names[i], "Jerry"));
+        }
+    }
+
+    printf("Not Found");
+    return 1;
+} 
